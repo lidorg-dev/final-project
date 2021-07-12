d6448951d604e09a8c5e6cd2c57fe7b6b1dc7b93
yg <yoav-g@bynet.co.il>
2021-07-12 16:36:47 +0300

    YG syntax Done for strings.py

--- strings.py
+++ strings.py
@@ -7,20 +7,42 @@ Create a function that returns the words in reverse order:
 Example: ['circus', 'flying', 'pythons', 'monty']
 Create a function that returns a list of 4 character strings:
 Example: ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
-### git comment
+### git comment    --> YG DONE ;-)
 """
+import textwrap as tr
+
 import pytest
 
+# import re
+
 def no_duplicates(a_string):
-    pass
+    ans = ''.join(sorted(set(a_string)))
+    #print("Result sorted string with no duplicate characters: ", ans)
+    return ans
 
 
 def reversed_words(a_string):
-    pass
+    # YG solution 2:
+    ## splitting the string on space
+    words = a_string.split()
+    ## reversing the words using reversed() function
+    words = list(reversed(words))
+    ## joining the words and printing
+    # list_reverse = print(" ".join(words))
+    return words
+#YG solution 1: but BUG with test_reversed_words()...
+    # # first split the string into words
+    # words = a_string.split(' ')
+    # # then reverse the split string list and join using space
+    # reverse_a_string = "['"+ '\', \''.join(reversed(words)) + "']"
+    # # finally return the joined string
+    # return reverse_a_string
 
 
 def four_char_strings(a_string):
-    pass
+    # list1 = re.findall('....', a_string)   # Good but 2 last caracteres missing...4,4,4,2...!
+    lines = tr.wrap(a_string, width=4)
+    return lines
 
 
 def test_no_duplicates():
@@ -35,13 +57,25 @@ def test_reversed_words():
 
 def test_four_char_strings():
     s = 'monty pythons flying circus'
-    assert four_char_strings(s) == ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
+    assert four_char_strings(s) == ['mont', 'y py', 'thon', 's fl', 'ying', 'circ', 'us']
+
+a_string = input("insert String:")
+print("Result sorted string with no duplicate characters: ", no_duplicates(a_string))
+
+print("Result that returns the words in reverse order: ", reversed_words(a_string))
+
+print("Result that returns a list of 4 character strings: ", four_char_strings(a_string))
+
+test_no_duplicates()
+test_reversed_words()
+test_four_char_strings()
 
 
+"""
 def main():
     return pytest.main(__file__)
 
 
 if __name__ == '__main__':
     main()
-    
+"""
