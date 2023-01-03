bff66c19d067038123989abce03ad9aea7dc23b0
aviazo <114329141+aviazo@users.noreply.github.com>
2023-01-03 08:38:20 +0200

    Commit  to the aviazo_sol branch

--- StateCap.py
+++ StateCap.py
@@ -13,7 +13,7 @@ handle that?
 """
 import sys
 
-import pytest
+#import pytest
 
 STATES_CAPITALS = {
     'Alabama' : 'Montgomery',
@@ -70,28 +70,55 @@ STATES_CAPITALS = {
 
 
 def capital_of_Idaho():
-    # Your code here
-    pass
+    print("the capital of Idaho is:", STATES_CAPITALS['Idaho'])
+        
+capital_of_Idaho()
+print ("\n")
 
 def all_states():
-    # Your code here
-    pass
+    print ("The lists of the states is:")
+    [print(keys) for keys in STATES_CAPITALS]
+    
+
+all_states()
+print ("\n")
 
 def all_capitals():
-    # Your code here
-    pass
+    print ("The list of thr capitals is:")
+    for keys, value in STATES_CAPITALS.items():
+        print(value)
+
+
+all_capitals()
+print ("\n")
 
 def states_capitals_string():
-    # Your code here
-    pass
+    
+    sorted_STATES_CAPITALS = sorted(STATES_CAPITALS.items(), key=lambda x:x[0])
+    
+    str = ""
+        
+    print ("The sort list of the states and capitals is:")
+
+    for keys, value in sorted_STATES_CAPITALS:
+        str += keys + '->' + value + ' , '
+      
+    print(str)
+    
+states_capitals_string()
+print ("\n")
+
+def get_state():
+    capital = input('Enter a capital to find the state: ')
+    for value , key in STATES_CAPITALS.items():
+        if capital in key:
+            print('The state is :' , value)
+   
+  
+get_state()
 
 
-
-def get_state(capital):
-    pass
-
-
-
+'''
 def test_state_to_capital():
     assert 'Cheyenne' == STATES_CAPITALS['Wyoming']
 
@@ -116,3 +143,4 @@ def main():
 
 if __name__ == '__main__':
     sys.exit(main())
+    '''
