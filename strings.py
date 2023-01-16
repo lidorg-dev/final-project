87ee2da193a0ceb9eaa567111594399cd765b13c
hur-roth <112489419+hur-roth@users.noreply.github.com>
2023-01-16 10:20:13 +0200

    Update strings.py
    fix code

--- strings.py
+++ strings.py
@@ -12,16 +12,19 @@ Example: ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
 import pytest
 
 def no_duplicates(a_string):
-    pass
-
+    temp = ''
+    for i in a_string:
+        if i not in temp:
+            temp += i
+    return ''.join(sorted(temp))
 
 def reversed_words(a_string):
-    pass
+    return a_string.split()[::-1]
 
 
 def four_char_strings(a_string):
-    pass
-
+    #print([a_string[i:i+4] for i in range(0, len(a_string), 4)])
+    return [a_string[i:i+4] for i in range(0, len(a_string), 4)]
 
 def test_no_duplicates():
     s = 'monty pythons flying circus'
