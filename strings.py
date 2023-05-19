7e6ede81afc9ad567f43de4f93c6dc4e43fa79ad
Lior Lavi <liorlavi1@gmail.com>
2023-05-19 15:19:30 +0300

    finished strings file

--- strings.py
+++ strings.py
@@ -9,17 +9,40 @@ Create a function that returns a list of 4 character strings:
 Example: ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
 ### git comment
 """
-import pytest
 
 def no_duplicates(a_string):
+    sort = ""
+    for word in a_string:
+        if word == "":
+            sort = sort-word
+        if word not in sort:
+            sort = sort+word
+    new = sorted(sort)
+    test=''.join(new)
+    print(test)
     pass
 
 
 def reversed_words(a_string):
+    newstring = a_string.split()
+    reversed = newstring[::-1]
+    print(reversed)
     pass
 
 
 def four_char_strings(a_string):
+    length = len(a_string)
+    i = 0
+    templist = ''
+    newlist = []
+    while i!=length:
+        templist = templist+a_string[i]
+        i += 1
+        if i % 4 == 0:
+            newlist.append(templist)
+            templist = ''
+    newlist.append(templist)
+    print(newlist)
     pass
 
 
@@ -37,11 +60,6 @@ def test_four_char_strings():
     s = 'monty pythons flying circus'
     assert four_char_strings(s) == ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
 
-
-def main():
-    return pytest.main(__file__)
-
-
-if __name__ == '__main__':
-    main()
-    
+no_duplicates('monty pythons flying circus')
+reversed_words('monty pythons flying circus')
+four_char_strings('monty pythons flying circus')
