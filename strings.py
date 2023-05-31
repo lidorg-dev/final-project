658b53d4e0cd8eea7cb63b92db9b83cd804f0f2e
YuvalSal99 <yuvalsal100@gmail.com>
2023-05-31 19:28:59 +0000

    last commit

--- strings.py
+++ strings.py
@@ -1,14 +1,4 @@
-"""With this string:
-'monty pythons flying circus'
-Create a function that returns a sorted string with no duplicate characters
-(keep any whitespace):
-Example: ' cfghilmnoprstuy'
-Create a function that returns the words in reverse order:
-Example: ['circus', 'flying', 'pythons', 'monty']
-Create a function that returns a list of 4 character strings:
-Example: ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
-### git comment
-"""
+
 """With this string:
 'monty pythons flying circus'
 Create a function that returns a sorted string with no duplicate characters
@@ -21,6 +11,7 @@ Example: ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
 ### git comment
 """
 
+import pytest
 
 string='monty pythons flying circus'
 
@@ -32,14 +23,14 @@ def no_duplicates(a_string):
                 sort = sort + i
     yep = sorted(sort)
     string=''.join(yep)
-    print(string)
+    return string
     pass
 
 
 def reversed_words(a_string):
      spl = a_string.split()
      reverse = spl[::-1]
-     print(reverse)
+     return reverse
      pass
 
 
@@ -50,23 +41,39 @@ def four_char_strings(a_string):
     print('Start:' + a_string)
     for i in a_string:
         temp = temp + i
-        if j == 3 :
+        if j == 3:
                 new_string.append(temp)
                 temp = ''
                 j = 0
         else:
             j = j + 1
     new_string.append(temp)
-    print(new_string)
+    return new_string
 
     pass
+
+def test_no_duplicates():
+    s = 'monty pythons flying circus'
+    assert no_duplicates(s) == 'cfghilmnoprstuy'
+
+
+def test_reversed_words():
+    s = 'monty pythons flying circus'
+    assert reversed_words(s) == ['circus', 'flying', 'pythons', 'monty']
+
+
+def test_four_char_strings():
+    s = 'monty pythons flying circus'
+    assert four_char_strings(s) == ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
 
 
 
 def main():
-    no_duplicates(string)
-    reversed_words(string)
-    four_char_strings(string)
+
+    return pytest.main(["strings.py"])
 
 if __name__ == '__main__':
     main()
+
+
+
